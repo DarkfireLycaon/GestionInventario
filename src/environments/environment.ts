c9115af2715fbf92c642b36d71d8d69739c3fb0a
Tu Nombre <rafatorretta@gmail.com>
2026-03-04 13:51:28 +0100

    chore: update api url for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/auth' // Tu backend local
+  production: true,
+  apiUrl: 'https://inventario-backend.onrender.com' // <-- AQUÍ PEGA TU URL DE RENDER
 };
