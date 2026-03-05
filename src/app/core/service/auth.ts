6af3c52e9f03c051627f795dfd5857ee7ede4ef0
Tu Nombre <rafatorretta@gmail.com>
2026-03-05 13:25:06 +0100

    ola

--- src/app/core/service/auth.ts
+++ src/app/core/service/auth.ts
@@ -7,7 +7,7 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-private apiUrl = environment.apiUrl; 
+private apiUrl = `${environment.apiUrl}/inventario-app/auth`;; 
   constructor(private http: HttpClient) { }
 
   login(creds: any): Observable<any> {
